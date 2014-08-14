7b40ad0a169af540a536b2a2b96e9f492a78ee33
leechannl <leechannl@gmail.com>
2014-08-14 22:23:04 +0800

    Initial Commit

---
+++ SimpleWeather/CLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLAppDelegate.h
+//  SimpleWeather
+//
+//  Created by mm on 8/14/14.
+//  Copyright (c) 2014 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleWeather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleWeather
+//
+//  Created by mm on 8/14/14.
+//  Copyright (c) 2014 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLAppDelegate class]));
+    }
+}
